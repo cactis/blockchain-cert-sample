33dfc9c4078395e9e55fd6f124dd698d793859ba
cactis <cactis.lin@gmail.com>
2017-06-19 11:08:05 +0800

    dummy

--- test/init.sh
+++ test/init.sh
@@ -9,24 +9,23 @@ npm install -g yo
 npm install -g composer-playground
 
 docker kill $(docker ps -q)
+# docker rm $(docker ps -aq)
+# docker rmi $(docker images dev-* -q)
 
-docker rm $(docker ps -aq)
-
-docker rmi $(docker images dev-* -q)
-
-mkdir ~/fabric-tools && cd ~/fabric-tools
+mkdir ~/fabric-tools
+pushd ~/fabric-tools
 curl -O https://raw.githubusercontent.com/hyperledger/composer-tools/master/packages/fabric-dev-servers/fabric-dev-servers.zip
 
 export FABRIC_VERSION=hlfv1
 
-cd ~/fabric-tools
+# pushd ~/fabric-tools
 ./downloadFabric.sh
 ./startFabric.sh
 ./createComposerProfile.sh
 
-cd ~/fabric-tools
 ./stopFabric.sh
 ./teardownFabric.sh
+popd
 
-cd ~/_bcs
-git clone https://github.com/cactis/blockchain-cert-sample
+# cd ~/_bcs
+# git clone https://github.com/cactis/blockchain-cert-sample
