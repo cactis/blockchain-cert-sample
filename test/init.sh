bf7780b7003211002873b03844e92e536d2af4f7
cactis <cactis.lin@gmail.com>
2017-06-19 11:55:39 +0800

    dummy

--- test/init.sh
+++ test/init.sh
@@ -1,5 +1,5 @@
 
-brew install node
+# brew install node
 node -v
 npm -v
 npm install -g composer-cli
@@ -8,24 +8,20 @@ npm install -g composer-rest-server
 npm install -g yo
 npm install -g composer-playground
 
-docker kill $(docker ps -q)
-# docker rm $(docker ps -aq)
-# docker rmi $(docker images dev-* -q)
+# docker kill $(docker ps -q)
+# # docker rm $(docker ps -aq)
+# # docker rmi $(docker images dev-* -q)
 
-mkdir ~/fabric-tools
-pushd ~/fabric-tools
-curl -O https://raw.githubusercontent.com/hyperledger/composer-tools/master/packages/fabric-dev-servers/fabric-dev-servers.zip
-
-export FABRIC_VERSION=hlfv1
-
+# mkdir ~/fabric-tools
 # pushd ~/fabric-tools
-./downloadFabric.sh
-./startFabric.sh
-./createComposerProfile.sh
+# curl -O https://raw.githubusercontent.com/hyperledger/composer-tools/master/packages/fabric-dev-servers/fabric-dev-servers.zip
+# unzip fabric-dev-servers.zip
 
-./stopFabric.sh
-./teardownFabric.sh
-popd
+# export FABRIC_VERSION=hlfv1
 
-# cd ~/_bcs
-# git clone https://github.com/cactis/blockchain-cert-sample
+# # pushd ~/fabric-tools
+
+# popd
+
+# # cd ~/_bcs
+# # git clone https://github.com/cactis/blockchain-cert-sample
